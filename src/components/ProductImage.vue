6518b729ef0291a26aefe2634fc7bdc30bd2f24b
HOK Sochetra <chetrafc60@gmail.com>
2023-12-15 18:15:59 +0700

    a

---
+++ src/components/ProductImage.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="img"
+        style="height: 100%; width: 100%; padding: 2%; box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;border-radius: 5px;">
+        <img src="@/assets/img/corn.png" style="width: 100%; height: 100%; object-fit: contain;" alt="">
+    </div>
+</template>
---
+++ src/components/Promotion.vue
@@ -0,0 +1,62 @@
+<template>
+    <div >
+        <RouterLink :to="productId">
+            <div class="promotion" :style="{backgroundColor:bg_color}">
+                <div class="content">
+                    <div style="font-weight: bold; font-size: large;" class="title">{{ title }}</div>
+                    <Button_show :button_color="btn_color" style="margin-top: 20px;"/>
+                </div>
+                <div class="image"><img :src="img" alt=""></div>
+            </div>
+        </RouterLink>
+    </div>
+</template>
+
+<script scope>
+
+    import Button_show from './Button_show.vue';
+    
+    export default{
+        name:'promotion',
+
+        components:{
+            Button_show,
+        },
+        props:{
+            img:String,
+            title:String,
+            btn_color:String,
+            bg_color:String,
+            productId:String,
+        },
+        
+    }
+</script>
+<style>
+    .promotion{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 280px;
+        border-radius: 10px;
+    }
+    .promotion .content{
+        width: 50%;
+        padding: 10% 0 10% 10%;
+        font-weight: 600;
+        font-size: 20px;
+    }
+    
+    .promotion .image{
+        height: 100%;
+        width: 50%;
+        display: flex;
+        align-items:flex-end;
+        justify-content: center;
+    }
+    .promotion img{
+        width: 100%;
+        object-fit: cover;
+        border-radius: 0 0 10px 0;
+    }
+</style>
